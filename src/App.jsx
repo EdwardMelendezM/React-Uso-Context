a577f712cea8c10b8431ccb1979a5bf6faa52475
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-02-09 09:27:26 -0500

    Usando useReducer y sus componentes, sate, dispatch

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import MyPage from "./components/Mypage";
 import CrudApi from "./components/CrudApi";
 import MyPageContext from "./components/MypageContext";
 import { CrudProvider } from "./context/CrudContext";
+import Contador from "./components/Contador";
 
 function App() {
   return (
     <div>
+      <h1>React reducer</h1>
+      <hr />
+      <Contador />
+      <hr />
       <h1>React context api</h1>
       <a
         href="https://es.reactjs.org/docs/context"
